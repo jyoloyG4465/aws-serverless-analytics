957b57a0056ebff960e684780d2351f815a37b49
jyoloyG <sriki1552@gmail.com>
2026-01-10 22:38:14 +0900

    命名変更

--- front-end/src/app/layout.tsx
+++ front-end/src/app/layout.tsx
@@ -14,7 +14,7 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "Aws serverless analytics",
+  title: "Youtube Analytics",
 };
 
 export default function RootLayout({
